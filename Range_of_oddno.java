f7100a11b6128bcca95a98a03ed9ab3eaf43bf1d
jenithavalarmathi <jenithakulandaivel@gmail.com>
2017-07-10 21:38:40 +0530

    Create Range_of_oddno.java

---
+++ Range_of_oddno.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+public class Range_of_oddno{
+  public static void main(String args[]){
+    Scanner s=new Scanner(System.in);
+    int n1,n2,i;
+    System.out.println("Enter the number of range in n1 to n2:");
+    n1=s.nextInt();
+    n2=s.nextInt();
+    for(i=n1;i<n2;i++){
+      if(i%2!=0){
+        
+        System.out.println(i);
+        }
+        }
+        }
+        }
+
+      
+      
+      
+      
+    
